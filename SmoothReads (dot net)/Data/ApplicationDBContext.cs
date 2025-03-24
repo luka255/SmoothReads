e94bbb135166652bee74b6d8e00c50f4f0be6ed7
luka khanjaladze <lkhanjaladze@gmail.com>
2025-03-24 19:08:13 +0400

    added database and models

---
+++ SmoothReads (dot net)/Data/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmoothReads.Models;
+
+namespace SmoothReads.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+           
+        }
+
+        public DbSet<User> users { get; set; }
+        public DbSet<Book> books { get; set; }
+        public DbSet<Comment> comments { get; set; }
+        public DbSet<Read> Reads { get; set; }
+        public DbSet<WantToRead> WantToReads { get; set; }
+        public DbSet<Favourite> favourites { get; set; }
+    }
+}
---
+++ SmoothReads (dot net)/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace SmoothReads.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password {  get; set; } = string.Empty;
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+        public List<Read> Reads { get; set; } = new List<Read>();
+        public List<WantToRead> WantsToReads { get; set; } = new List<WantToRead>();
+        public List<Favourite> Favourites { get; set; } = new List<Favourite>();
+    }
+}
